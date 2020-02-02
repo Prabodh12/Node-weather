da4cc8d1b902cac71db6e9d283f3d362b2f0faee
Prabodh <prabodh.shewalkar@gmail.com>
2020-02-02 16:04:29 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -91,6 +91,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("server strated")
+app.listen(port, () => {
+    console.log("server strated at "+port)
 })
